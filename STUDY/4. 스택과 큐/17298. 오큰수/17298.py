8fdf97a1f2b1ffee394c86ca562bac29a1b5f764
blue4683 <blue4683>
2023-02-15 17:01:37 +0900

    solve BAEKJOON ONLINE JUDGE - 17298. 오큰수

--- STUDY/4. 스택과 큐/17298. 오큰수/17298.py
+++ STUDY/4. 스택과 큐/17298. 오큰수/17298.py
@@ -1,6 +1,56 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
+
+n=int(input())
+arr=list(map(int,input().split()))
+stack=[]
+result=[-1]*n
+for i in range(n):
+    while stack and arr[stack[-1]]<arr[i]:
+        result[stack.pop()]=arr[i]
+    stack.append(i)
+print(*result)
+# 2차
+# def search(st,ed,v):
+#     for j in range(st,ed):
+#         if v<arr[j]:
+#             tmp=(j,arr[j])
+#             stack.append(tmp)
+#             result.append(tmp[1])
+#             return
+#     else:
+#         result.append(-1)
+#     return
+
+# n=int(input())
+# arr=list(map(int,input().split()))
+# stack=deque([])
+# result=[]
+# for idx in range(n):
+#     value=arr[idx]
+#     if not stack:
+#         search(idx+1,n,value)
+#         continue
+#     while stack:
+#         tmp=stack.popleft()
+#         if tmp[0]<idx:
+#             continue
+#         else:
+#             if tmp[1]>value:
+#                 for j in range(idx+1,tmp[0]):
+#                     if value<arr[j]:
+#                         stack.append((j,arr[j]))
+#                         result.append(arr[j])
+#                         break
+#                 else:
+#                     result.append(tmp[1])
+#                 stack.appendleft(tmp)
+#                 break
+#     else:
+#         search(idx+1,n,value)
+#         continue
+# print(*result)
+
 
 # 1차
 # def search(value,idx):
