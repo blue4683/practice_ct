a03a775ec60992dac789e472f3e4de2f5d5c6c54
안경준 <an4683@gmail.com>
2023-03-23 09:01:48 +0900

    solve BAEKJOON ONLINE JUDGE - 1920. 수 찾기

---
+++ STUDY/6. 탐색/1920. 수 찾기/1920.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n=int(input())
+numList=set(map(int, input().split()))
+m=int(input())
+targetList=list(map(int,input().split()))
+
+for target in targetList:
+    if target in numList:
+        print(1)
+    else:
+        print(0)
