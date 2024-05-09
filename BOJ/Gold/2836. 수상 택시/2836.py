ec658af2c28650cc79150caf9a0dbed89583dd8b
blue4683 <an4683@gmail.com>
2024-05-09 15:38:25 +0900

    solve BAEKJOON ONLINE JUDGE - 2836. 수상 택시

---
+++ BOJ/Gold/2836. 수상 택시/2836.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+back = []
+for _ in range(n):
+    a, b = map(int, input().split())
+    if a > b:
+        back.append((b, a))
+
+back.sort()
+arrival = back[0][1]
+distance = back[0][1] - back[0][0]
+
+for start, end in back[1:]:
+    if arrival >= start:
+        if arrival < end:
+            distance += end - arrival
+            arrival = end
+
+    else:
+        distance += end - start
+        arrival = end
+
+print(distance * 2 + m)
