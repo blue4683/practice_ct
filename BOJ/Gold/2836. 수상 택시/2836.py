aebddfd55a25d75252834d2e7784397bb957901b
blue4683 <an4683@gmail.com>
2024-09-11 14:22:18 +0900

    edited due to regrading

--- BOJ/Gold/2836. 수상 택시/2836.py
+++ BOJ/Gold/2836. 수상 택시/2836.py
@@ -8,18 +8,22 @@ for _ in range(n):
     if a > b:
         back.append((b, a))
 
-back.sort()
-arrival = back[0][1]
-distance = back[0][1] - back[0][0]
+if not back:
+    print(m)
 
-for start, end in back[1:]:
-    if arrival >= start:
-        if arrival < end:
-            distance += end - arrival
+else:
+    back.sort()
+    arrival = back[0][1]
+    distance = back[0][1] - back[0][0]
+
+    for start, end in back[1:]:
+        if arrival >= start:
+            if arrival < end:
+                distance += end - arrival
+                arrival = end
+
+        else:
+            distance += end - start
             arrival = end
 
-    else:
-        distance += end - start
-        arrival = end
-
-print(distance * 2 + m)
+    print(distance * 2 + m)
