70af72a87369a71d2cca4877b3e5787f99141ca0
blue4683 <an4683@gmail.com>
2026-03-14 16:52:02 +0900

    solve BAEKJOON ONLINE JUDGE - 24480. 알고리즘 수업 - 깊이 우선 탐색 2

---
+++ BOJ/Silver/24480. 알고리즘 수업 - 깊이 우선 탐색 2/24480.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
+
+
+def dfs(now):
+    global cnt
+    for node in graph[now]:
+        if visited[node]:
+            continue
+
+        visited[node] = cnt
+        cnt += 1
+        dfs(node)
+
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(1, n + 1):
+    graph[i].sort(reverse=True)
+
+visited = [0] * (n + 1)
+visited[r] = 1
+cnt = 2
+dfs(r)
+
+for i in range(1, n + 1):
+    print(visited[i])
