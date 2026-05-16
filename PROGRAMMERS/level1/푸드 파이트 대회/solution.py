32f6a18d9085277ace8b04a426028b145f319168
blue4683 <an4683@gmail.com>
2026-05-16 15:55:58 +0900

    solve Programmers - Level 1. 푸드 파이트 대회

---
+++ PROGRAMMERS/level1/푸드 파이트 대회/solution.py
@@ -0,0 +1,8 @@
+def solution(food):
+    answer = ''
+    tmp = ''
+    for i in range(1, len(food)):
+        tmp += str(i) * (food[i] // 2)
+
+    answer = tmp + '0' + tmp[::-1]
+    return answer
