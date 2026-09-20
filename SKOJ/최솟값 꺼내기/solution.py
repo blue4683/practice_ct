252b23c0c2208715acae60d36de4f6505e002132
blue4683 <an4683@gmail.com>
2026-09-20 11:42:15 +0900

    solve SKOJ - 최솟값 꺼내기

---
+++ SKOJ/최솟값 꺼내기/solution.py
@@ -0,0 +1,13 @@
+from heapq import heappush, heappop
+
+
+n = int(input())
+heap = []
+for _ in range(n):
+    x = int(input())
+    if not x:
+        print(0) if not heap else print(heappop(heap))
+        
+    else:
+        heappush(heap, x)
+        
